4fd71684489ad2d6172eaa6bc82edcbfc12bff0d
Sander Rijken <sander.rijken@sdbgroep.nl>
2026-03-03 12:35:06 +0100

    back to oidc client js

--- src/helpers/createUserManager.js
+++ src/helpers/createUserManager.js
@@ -1,4 +1,4 @@
-import * as OidcClient from 'oidc-client-ts';
+import OidcClient from '@competencegroup/oidc-client/index.js';
 const UserManager = OidcClient.UserManager;
 
 export default function createUserManager(config) {
